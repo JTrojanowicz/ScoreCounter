ab5a5bcb2482481fb2d406b78427858b25b38105
JarekRES <j.trojanowicz@res.rh.pl>
2021-11-20 19:34:33 +0100

    Initial Commit

---
+++ ScoreCounter/ScoreCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreCounterApp.swift
+//  ScoreCounter
+//
+//  Created by Jaroslaw Trojanowicz on 20/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScoreCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
